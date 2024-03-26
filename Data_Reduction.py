af6d357bda581f2eac95aec4d2b26ea2f5770bde
Arefay97 <127543670+Arefay97@users.noreply.github.com>
2024-03-26 09:42:25 +0100

    removing selfloops

--- Data_Reduction.py
+++ Data_Reduction.py
@@ -1,17 +1,36 @@
 import utils
 
-def Remove_cycles(G):
-    '''Function Description'''
+def Remove_selfloops(edges):
+    '''Function Descrition'''
 
     
+    #deleting 179 self loops
+    #list of rows that should be droped
+    index_to_drop = []
+    #iterate over each row
+    for index_, row in edges.iterrows():
+        #get source and target values
+        source = row['source']
+        target = row['target']
+        #compare the source and target if they are equal
+        if source == target:
+            #save the index of each row that should be deleted
+            index_to_drop.append(index_)
+    
+    #drop rows in the list
+    edges = edges.drop(edges.index[index_to_drop])
+
+    return edges
+
+#def Remove_reciprocal_edges():
+    '''Funcrion Description'''
+
 
 
 
 
 def Reduce_with_min_x_edges(G , target_degree):
     '''Function Describtion'''
-
-
 
     #nodes with the target degree
     nodes_to_remove = [node for node, degree in dict(G.degree()).items() if degree == target_degree]
